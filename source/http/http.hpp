5630bd24c94f835d86f29418c146e4baf735744b
赵泽浩 <3516422381@qq.com>
2026-08-25 15:18:45 +0800

    请求应答报文格式实现

--- source/http/http.hpp
+++ source/http/http.hpp
@@ -323,3 +323,139 @@ class Util {
             return true;
         }
 };
+class HttpRequest {
+    public:
+        std::string _method;      //请求方法
+        std::string _path;        //资源路径
+        std::string _version;     //协议版本
+        std::string _body;        //请求正文
+        std::smatch _matches;     //资源路径的正则提取数据
+        std::unordered_map<std::string, std::string> _headers;  //头部字段
+        std::unordered_map<std::string, std::string> _params;   //查询字符串
+    public:
+        HttpRequest():_version("HTTP/1.1") {}
+        void ReSet() {
+            _method.clear();
+            _path.clear();
+            _version = "HTTP/1.1";
+            _body.clear();
+            std::smatch match;
+            _matches.swap(match);
+            _headers.clear();
+            _params.clear();
+        }
+        //插入头部字段
+        void SetHeader(const std::string &key, const std::string &val) {
+            _headers.insert(std::make_pair(key, val));
+        }
+        //判断是否存在指定头部字段
+        bool HasHeader(const std::string &key) const {
+            auto it = _headers.find(key);
+            if (it == _headers.end()) {
+                return false;
+            }
+            return true;
+        }
+        //获取指定头部字段的值
+        std::string GetHeader(const std::string &key) const {
+            auto it = _headers.find(key);
+            if (it == _headers.end()) {
+                return "";
+            }
+            return it->second;
+        }
+        //插入查询字符串
+        void SetParam(const std::string &key, const std::string &val) {
+            _params.insert(std::make_pair(key, val));
+        }
+        //判断是否有某个指定的查询字符串
+        bool HasParam(const std::string &key) const {
+            auto it = _params.find(key);
+            if (it == _params.end()) {
+                return false;
+            }
+            return true;
+        }
+        //获取指定的查询字符串
+        std::string GetParam(const std::string &key) const {
+            auto it = _params.find(key);
+            if (it == _params.end()) {
+                return "";
+            }
+            return it->second;
+        }
+        //获取正文长度
+        size_t ContentLength() const {
+            // Content-Length: 1234\r\n
+            bool ret = HasHeader("Content-Length");
+            if (ret == false) {
+                return 0;
+            }
+            std::string clen = GetHeader("Content-Length");
+            return std::stol(clen);
+        }
+        //判断是否是短链接
+        bool Close() const {
+            // 没有Connection字段，或者有Connection但是值是close，则都是短链接，否则就是长连接
+            if (HasHeader("Connection") == true && GetHeader("Connection") == "keep-alive") {
+                return false;
+            }
+            return true;
+        }
+};
+
+class HttpResponse {
+    public:
+        int _statu;
+        bool _redirect_flag;
+        std::string _body;
+        std::string _redirect_url;
+        std::unordered_map<std::string, std::string> _headers;
+    public:
+        HttpResponse():_redirect_flag(false), _statu(200) {}
+        HttpResponse(int statu):_redirect_flag(false), _statu(statu) {} 
+        void ReSet() {
+            _statu = 200;
+            _redirect_flag = false;
+            _body.clear();
+            _redirect_url.clear();
+            _headers.clear();
+        }
+        //插入头部字段
+        void SetHeader(const std::string &key, const std::string &val) {
+            _headers.insert(std::make_pair(key, val));
+        }
+        //判断是否存在指定头部字段
+        bool HasHeader(const std::string &key) {
+            auto it = _headers.find(key);
+            if (it == _headers.end()) {
+                return false;
+            }
+            return true;
+        }
+        //获取指定头部字段的值
+        std::string GetHeader(const std::string &key) {
+            auto it = _headers.find(key);
+            if (it == _headers.end()) {
+                return "";
+            }
+            return it->second;
+        }
+        void SetContent(const std::string &body,  const std::string &type = "text/html") {
+            _body = body;
+            SetHeader("Content-Type", type);
+        }
+        void SetRedirect(const std::string &url, int statu = 302) {
+            _statu = statu;
+            _redirect_flag = true;
+            _redirect_url = url;
+        }
+        //判断是否是短链接
+        bool Close() {
+            // 没有Connection字段，或者有Connection但是值是close，则都是短链接，否则就是长连接
+            if (HasHeader("Connection") == true && GetHeader("Connection") == "keep-alive") {
+                return false;
+            }
+            return true;
+        }
+};
